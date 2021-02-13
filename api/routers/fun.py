21dc3e3d94492024dcaf9bfef91e715de1b40cf5
janaSunrise <warriordefenderz@gmail.com>
2021-02-13 13:08:39 +0530

    Work on yesno and why endpoints.

--- api/routers/fun.py
+++ api/routers/fun.py
@@ -35,3 +35,37 @@ async def chucknorris():
     return {
         "joke": json["value"]
     }
+
+
+@router.get("/why")
+async def why():
+    async with http_client.session.get("https://nekos.life/api/why") as resp:
+        json = await resp.json()
+
+    return {
+        "why": json["why"]
+    }
+
+
+@router.get("/yesno")
+async def yesno(question: str):
+    async def get_answer(self, ans: str) -> str:
+        return_str = ""
+        if ans == "yes":
+            return_str = "Yes."
+        elif ans == "no":
+            return_str = "NOPE"
+        elif ans == "maybe":
+            return_str = "maaaaaaybe?"
+        else:
+            return_str = "Internal Error: Invalid answer LMAOO"
+
+        return return_str
+
+    async with http_client.session.get("https://yesno.wtf/api") as resp:
+        json = await resp.json()
+
+    return {
+        "question": question,
+        "answer": get_answer(json["answer"])
+    }
