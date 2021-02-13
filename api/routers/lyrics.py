dbab3409ce938b8474d99b6e9f722cb2ac9cb89a
janaSunrise <warriordefenderz@gmail.com>
2021-02-13 22:03:46 +0530

    Add lyrics endpoint.

---
+++ api/routers/lyrics.py
@@ -0,0 +1,27 @@
+from fastapi import APIRouter
+
+from api.app import http_client
+
+router = APIRouter(
+    prefix="/lyrics",
+    tags=["lyrics"],
+    responses={
+        404: {"description": "Not found"},
+    },
+)
+
+
+# -- Router paths --
+@router.get("/")
+async def lyrics(songname: str):
+    """Get the lyrics for some song which you need."""
+    async with http_client.session.get(f"https://some-random-api.ml/lyrics?title={songname}") as resp:
+        json = await resp.json()
+
+    return {
+        "title": json["title"],
+        "author": json["author"],
+        "lyrics": json["lyrics"],
+        "thumbnail": json["thumbnail"]["genius"]
+    }
+
