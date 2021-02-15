399920252ed60cdcfe9870e06753ed9bd148813b
janaSunrise <warriordefenderz@gmail.com>
2021-02-15 10:50:58 +0530

    Add ratelimiting to lyrics endpoint..

--- api/routers/lyrics.py
+++ api/routers/lyrics.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Request
 
 from api import http_client
 from api.core import log_error
@@ -15,7 +15,7 @@ router = APIRouter(
 # -- Router paths --
 @router.get("/")
 @log_error()
-async def lyrics(songname: str):
+async def lyrics(request: Request, songname: str):
     """Get the lyrics for some song which you need."""
     async with http_client.session.get(f"https://some-random-api.ml/lyrics?title={songname}") as resp:
         json = await resp.json()
