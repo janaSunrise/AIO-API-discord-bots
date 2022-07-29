4c8b79a8cd4202c83430bf7c2dacfb17bc7286de
janaSunrise <warriordefenderz@gmail.com>
2022-07-29 22:49:55 +0530

    Add the JSON files with data and load them in configuration.

--- api/config.py
+++ api/config.py
@@ -1,3 +1,4 @@
+import json
 from typing import cast
 
 from asyncpraw import Reddit, exceptions
@@ -36,3 +37,31 @@ try:
     )
 except exceptions.MissingRequiredAttributeException:
     logger.error("Please set correct reddit environment variables to run.")
+
+# Load all the JSON files
+with open("assets/subreddits.json") as f:
+    subreddits = json.load(f)
+
+with open("assets/nsfw_subreddits.json") as f:
+    nsfw_subreddits = json.load(f)
+
+with open("assets/text_game_responses.json") as f:
+    text_game_responses = json.load(f)
+
+# Study router utils
+RESPONSES = {
+    200: True,
+    301: "Switching to a different endpoint",
+    400: "Bad Request",
+    401: "Not Authenticated",
+    404: "The resource you tried to access wasn't found on the server.",
+    403: (
+        "The resource you're trying to access is forbidden — "
+        "you don't have the right permissions to see it."
+    ),
+}
+
+# Wolfram alpha API config
+DEFAULT_OUTPUT_FORMAT = "JSON"
+QUERY = "http://api.wolframalpha.com/v2/{request}?{data}"
+MAX_PODS = 20
