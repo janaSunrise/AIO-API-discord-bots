0efe1d2d64f324797f496f6a903c8047a3fd8da9
janaSunrise <warriordefenderz@gmail.com>
2022-08-01 23:10:59 +0530

    Added NASA endpoint.

---
+++ api/routers/nasa.py
@@ -0,0 +1,80 @@
+import random
+
+from fastapi import APIRouter, Request
+
+from ..config import APIConfig
+
+router = APIRouter(
+    prefix="/nasa",
+    tags=["NASA endpoint"],
+    responses={
+        404: {"description": "Not found"},
+    },
+)
+
+NASA_API = APIConfig.NASA_API
+
+
+@router.get("/apod")
+async def apod(request: Request) -> dict:
+    """Get the astronomy picture of the day."""
+    http_client = request.app.state.http_client
+
+    async with http_client().get(
+        f"https://api.nasa.gov/planetary/apod?api_key={NASA_API}"
+    ) as resp:
+        data = await resp.json()
+
+    return {
+        "title": data["title"],
+        "explanation": data["explanation"],
+        "img": data["hdurl"],
+    }
+
+
+@router.get("/nasa-search")
+async def nasa_search(request: Request, query: str) -> dict:
+    """Lookup nasa for your queries."""
+    http_client = request.app.state.http_client
+
+    async with http_client().get(
+        f"https://images-api.nasa.gov/search?q={query}"
+    ) as resp:
+        data = await resp.json()
+
+    items = data["collection"]["items"]
+    if len(items) > 0:
+        rand_item = random.randint(0, len(items) - 1)
+        item = items[rand_item]
+
+        return {
+            "description": item["data"][0]["description"],
+            "img": item["links"][0]["href"],
+            "id": item["data"][0]["nasa_id"],
+        }
+    return {"error": "No results found!"}
+
+
+@router.get("/epic")
+async def epic(request: Request, maximum: int = 1) -> dict:
+    """Get to know about a nasa EPIC."""
+    http_client = request.app.state.http_client
+
+    async with http_client().get(
+        "https://epic.gsfc.nasa.gov/api/images.php"
+    ) as response:
+        json = await response.json()
+
+    result = {}
+
+    for i in range(min(maximum, len(json))):
+        result[i] = {
+            "description": json[i].get("caption"),
+            "img": (
+                "https://epic.gsfc.nasa.gov/epic-archive/jpg/"
+                + json[i]["image"]  # noqa: W503
+                + ".jpg"  # noqa: W503
+            ),
+        }
+
+    return result
